c2bd5a008120365fe45164e64528af02a0fb9a63
its-khatana <khatana.rohit@tftus.com>
2024-05-27 16:45:46 +0530

    Part 15: How to Handle Hidden Items in DropDown (II)

--- tests/hiddenDropDown.spec.ts
+++ tests/hiddenDropDown.spec.ts
@@ -13,3 +13,19 @@ test('Handle Hidden DropDowns (Select Job Title as Automation Tester', async({pa
     await page.locator("//div[@role='option']//span[normalize-space()='Automaton Tester']").click()
     await page.waitForTimeout(3000);
 });
+
+
+test.only('Print sll the job roles', async({page})=>{
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    await page.locator("//input[@placeholder='Username']").fill('Admin')
+    await page.locator("//input[@placeholder='Password']").fill('admin123')
+    await page.locator("//button[normalize-space()='Login']").click()
+    await page.locator("(//span[normalize-space()='PIM'])[1]").click()
+    await page.locator("//label[normalize-space()='Job Title']/../..//div[contains(@class,'oxd-select-wrapper')]").click()
+    await page.waitForTimeout(5000);
+    const jobsLocators=await page.locator("//div[@role='option']//span[contains(text(),'')]").all()
+    for(let job of jobsLocators){
+        const jobTitle = await job.textContent();
+        console.log(jobTitle);
+    };
+});
